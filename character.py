7b2a1bd591410b451b7bf989aa7f123a9ee2da2b
Ali Abas Arsalahn <ali.abas.arsalahn@icloud.com>
2022-04-25 12:00:23 +0200

    added docstrings to: __repr__, __str__, level_setter, experience_getter, experience_setter level_up method, current_health_getter, speak_method, roll_dice method, dodge_chance_getter, dodge_chance_setter

--- character.py
+++ character.py
@@ -22,28 +22,38 @@ class Character(ABC):
         }
 
     def __repr__(self) -> str:
+        """must be implemented by every subclass"""
         pass
 
     def __str__(self) -> str:
+        """must be implemented by every subclass"""
         pass
 
     @property
     def level(self) -> int:
+        """Level getter."""
         return self.level
 
     @level.setter
     def level(self, value: int) -> None:
+        """Level setter"""
         self.level += value
 
     @property
     def experience(self) -> int:
+        """experience getter"""
         return self.experience
 
     @experience.setter
     def experience(self, value) -> None:
+        """Experience setter. Get's called after every combat."""
         self.experience += value
 
     def level_up(self, experience: int) -> None:
+        """
+        Get's called after every combat. Takes experience earned as Argument.
+        Increases player level if certain experience tresholds are met.
+        """
         pass
 
     @property
@@ -58,23 +68,31 @@ class Character(ABC):
 
     @property
     def current_health(self) -> int:
+        """Returns current health"""
         return self.current_health
 
     @current_health.setter
     def current_health(self, damage) -> None:
+        """decreases health if damage is sustained. Get's called during combat
+        on every enemy attack."""
         self.current_health -= damage
 
     @abstractmethod
     def speak() -> str:
+        """Needs an implementation by every subclass"""
         pass
 
     def roll_dice(max_range) -> int:
+        """returns a random number. Is used to determine damage value of 
+        every ability."""
         return randrange(1, max_range)
 
     @property
     def dodge_chance(self) -> float:
+        """dodge_chance getter"""
         return self.dodge_chance
 
     @dodge_chance.setter
     def dodge_chance(self) -> None:
+        """Dodge_chance get's set to 0.01% for each point of agility a character has."""
         self.dodge_chance = 0.01 * self.attributes["agility"]
