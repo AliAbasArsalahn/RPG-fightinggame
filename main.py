04c067db68d320c54ad76a9da7240855c1f630ef
Ali Abas Arsalahn <ali.abas.arsalahn@icloud.com>
2022-04-25 02:42:56 +0200

    added main module set WIDHT and HEIGHT to 1400, 900 set run loop

---
+++ main.py
@@ -0,0 +1,22 @@
+# Projekt: RPG-fightinggame \ Modul: main
+# Author: Ali Abas Arsalahn
+# Datum: 25.04.2022
+
+import pygame
+
+
+WIDTH, HEIGHT = 1400, 900
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main() -> None:
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
