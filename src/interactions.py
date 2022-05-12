33077638f03c616107743bb185d5a407db6a9bc1
Ali Abas Arsalahn <ali.abas.arsalahn@icloud.com>
2022-05-12 20:13:55 +0200

    created class Interacions; added method 'speak' added interaction as Subclass to Character

--- src/interactions.py
+++ src/interactions.py
@@ -6,13 +6,33 @@
 
 from random import randrange
 
-def roll_dice(max_range: int) -> int:
-    """Returns a random integer"""
-    return randrange(1, max_range)
+
+class Interactions:
+    """
+    Interactions class. Contains generic Methods, that characters can use.
+    Methods: roll_dice, combat, speak
+    """
+    def roll_dice(max_range: int) -> int:
+        """Returns a random integer"""
+        return randrange(1, max_range)
 
 
-def combat(object_1: object, object_2: object) -> None:
-    """Combat function."""
-    both_combatants_alive = True
-    while both_combatants_alive:
-        pass
+    def combat(object_1: object, object_2: object) -> None:
+        """Combat function."""
+        both_combatants_alive = True
+        while both_combatants_alive:
+            pass
+
+        def speak(self) -> tuple:
+        """checks intelligence attributes and allows speach acording to the stat."""
+        intelligence = self.attributes.get("intelligence")
+        if intelligence > 3:
+            can_speak_to_peasants = True
+        if intelligence > 5:
+            can_speak_to_citizens = True
+        if intelligence > 8:
+            can_speak_to_nobles = True
+
+        speech = (can_speak_to_peasants,
+                  can_speak_to_citizens, can_speak_to_nobles)
+        return speech
