b66857fcdca3bbd0c48274c212b50a48fab82a53
Ali Abas Arsalahn <ali.abas.arsalahn@icloud.com>
2022-06-05 19:01:22 +0200

    created Button class; created display.py module

--- src/interface.py
+++ src/interface.py
@@ -1,6 +1,10 @@
 """main_menu.py"""
 import pygame
 
+screen = pygame.display.set_mode((800, 800))
+pygame.display.set_caption("Button!")
+main_font = pygame.font.SysFont("cambria", 50)
+
 
 class Button():
     """Button class"""
