b1657ec1f249181810cf2ec4ce4aa31de9ea6aaa
Ali Abas Arsalahn <ali.abas.arsalahn@icloud.com>
2022-06-05 19:01:12 +0200

    created Button class; created display.py module

---
+++ src/interface.py
@@ -0,0 +1,22 @@
+"""main_menu.py"""
+import pygame
+
+
+class Button():
+    """Button class"""
+
+    def __init__(self, image, x_pos, y_pos, text_input) -> None:
+        self.image = image
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+        self.text_input = text_input
+        self.text = main_font.render(self.text_input, True, "white")
+        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+
+    def update(self) -> None:
+        screen.blit(self.image, self.rect)
+        screen.blit(self.text, self.text_rect)
+
+    def check_for_input(self, position) -> None:
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top)
