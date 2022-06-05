4c44fa4362c8499a300d8531849c247911d87492
Ali Abas Arsalahn <ali.abas.arsalahn@icloud.com>
2022-06-06 00:41:08 +0200

    placed window methods from main.py into seperate class; placed inside display.py module

--- src/display.py
+++ src/display.py
@@ -6,14 +6,31 @@ import pygame
 class Display():
     """Display class; inherits from pygame.display"""
 
-    def __init__(self, height: int, width: int, title: str) -> None:
+    def __init__(self, height: int, width: int, title: str, fps: int) -> None:
         self.height = height
         self.width = width
         self.title = title
         self.COLOURS = {
             "white": (255, 255, 255)
         }
+        self.WIN = pygame.display.set_mode((self.width, self.height))
+        self.FPS = fps
+        self.clock = pygame.time.Clock()
 
     def draw_window(self) -> None:
-        pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption(self.title)
+        self.WIN.fill()
+        self.WIN.blit()
+        pygame.display.update()
+
+    def window(self) -> None:
+        run = True
+        while run:
+            self.clock.tick(self.FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+            self.draw_window()
+
+        pygame.quit()
--- src/main.py
+++ src/main.py
@@ -1,39 +1,39 @@
-"""This module uses the pygame library to run the game"""
+"""main.py module"""
 
 
 import pygame
 
 
-WIDTH, HEIGHT = 1400, 900
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("RPG-fightinggame")
+# WIDTH, HEIGHT = 1400, 900
+# WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+# pygame.display.set_caption("RPG-fightinggame")
 
-WHITE = (255, 255, 255)
-RANDOM_COLOR = (140, 25, 55)
+# WHITE = (255, 255, 255)
+# RANDOM_COLOR = (140, 25, 55)
 
-FPS = 60
+# FPS = 60
 
 
-def draw_window() -> None:
-    """Set's display color to white"""
-    WIN.fill(WHITE)
-    WIN.blit()
-    pygame.display.update()
+# def draw_window() -> None:
+#     """Set's display color to white"""
+#     WIN.fill(WHITE)
+#     WIN.blit()
+#     pygame.display.update()
 
 
 def main() -> None:
     """main loop. Opens a window"""
-    clock = pygame.time.Clock()
-    run = True
-    while run:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
+    # clock = pygame.time.Clock()
+    # run = True
+    # while run:
+    #     clock.tick(FPS)
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             run = False
 
-        draw_window()
+    #     draw_window()
 
-    pygame.quit()
+    # pygame.quit()
 
 
 if __name__ == '__main__':
