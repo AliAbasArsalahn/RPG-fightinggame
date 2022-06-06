cd50dde47b9924f046e54db0d450352be8f40130
Ali Abas Arsalahn <ali.abas.arsalahn@icloud.com>
2022-06-07 00:19:18 +0200

    added type checking to controller.py

--- src/controller.py
+++ src/controller.py
@@ -1,8 +1,13 @@
 """controller module."""
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from character import Character
+
 
 class Controller:
     """Controller class"""
     @staticmethod
-    def health_increase(character: object, amount: int) -> None:
-        character.health += amount
+    def health_increase(character: Character, amount: int) -> None:
+        character.attributes.health += amount
