48040689ff6a7e5abdd811a16a3e324b68c9e0b4
Ali Abas Arsalahn <ali.abas.arsalahn@icloud.com>
2022-06-11 11:16:58 +0200

    seperated combat into another class

--- src/combat.py
+++ src/combat.py
@@ -1,1 +1,15 @@
 """combat module"""
+
+from typing import TYPE_CHECKING
+class Combat:
+    """Combat-Interactions class"""
+    @staticmethod
+    def spellbook(spells: dict) -> None:
+        """
+        lists available spells and allows to choose.
+        Takes available spells as argument.
+        
+        """
+        spell_index = 1
+        for spell in spells:
+            print(f" {spell_index} {spells[spell]}")
