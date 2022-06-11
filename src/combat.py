a56ce38af46920e9548d6bfbb74ca07b13f54ef8
Ali Abas Arsalahn <ali.abas.arsalahn@icloud.com>
2022-06-11 20:08:36 +0200

    -/-

--- src/combat.py
+++ src/combat.py
@@ -1,6 +1,10 @@
 """combat module"""
 
 from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from character import Character
+
+
 class Combat:
     """Combat-Interactions class"""
     @staticmethod
@@ -8,8 +12,15 @@ class Combat:
         """
         lists available spells and allows to choose.
         Takes available spells as argument.
-        
+
         """
+    def list_spellbooks(*args: list[Character]) -> None:
+        for character in args:
+            print(character.spellbook)
+
         spell_index = 1
         for spell in spells:
             print(f" {spell_index} {spells[spell]}")
+            spell_index += 1
+        spell_select = int(input("choose one ablity! "))
+        spells[spell_select]()
