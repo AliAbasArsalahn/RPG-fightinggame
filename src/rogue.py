8050a5ea2551162599b48965f1cfdf655ac1c6e8
Ali Abas Arsalahn <ali.abas.arsalahn@icloud.com>
2022-06-16 14:46:38 +0200

    added Rogue constructor; added Rogue.spells as dict; added Rogue.dagger_attack and Rogue.stealth to Rogue.spells

--- src/rogue.py
+++ src/rogue.py
@@ -14,6 +14,13 @@ class Rogue(Character):
     Methods: dagger_attack, stealth
     """
 
+    def __init__(self) -> None:
+        super().__init__()
+        self.spells = {
+            1: self.dagger_attack,
+            2: self.stealth
+        }
+
     def dagger_attack(self) -> int:
         """dagger_attack method. Calculates damage done and returns it as integer."""
         DAMAGE_MULIPLIER = 4
